132e301df873e9f12e889b3046c5efa85da805f9
Hoa <x5brain@gmail.com>
2022-10-08 20:22:20 +0800

    update

--- Sources/SwiftUIHud/SwiftUIHudState.swift
+++ Sources/SwiftUIHud/SwiftUIHudState.swift
@@ -19,6 +19,8 @@ public class SwiftUIHudState: ObservableObject {
     private(set) public var systemImage: String? = ""
     private(set) public var type: SwiftUIHudType = .progress()
     
+    public init() {}
+    
     public var alignment: Alignment {
         switch type {
         case .progress: return .center
